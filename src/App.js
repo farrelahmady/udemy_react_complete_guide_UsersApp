fbc3efaec1a288903162728f15d8046f7f6b0568
Farrel Ahmad Yudithia <farrelfay.ce@gmail.com>
2022-09-02 22:37:16 +0700

    UserList Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { AddUser } from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
 
 function App() {
+	const [users, setUsers] = React.useState([]);
 	return (
 		<div>
 			<AddUser />
+			<UsersList users={users} />
 		</div>
 	);
 }
