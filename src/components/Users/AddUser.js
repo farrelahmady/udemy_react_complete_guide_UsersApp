41281becb9bf44192f9ac41b705aaff6a6eb5299
Farrel Ahmad Yudithia <farrelfay.ce@gmail.com>
2022-09-02 22:29:51 +0700

    validation

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -3,19 +3,61 @@ import Card from "../UI/Card";
 import Button from "../UI/Button";
 
 import style from "./AddUser.module.css";
+import { useState } from "react";
 
 export const AddUser = () => {
+	const [username, setUsername] = useState("");
+	const [age, setAge] = useState("");
+
 	const addUserHandler = (event) => {
 		event.preventDefault();
+
+		//* Validation
+		if (username.trim().length === 0 || age.trim().length === 0) {
+			console.log("Please fill in all fields");
+			return;
+		}
+		if (+age < 1) {
+			console.log("Age must be a positive number");
+			return;
+		}
+
+		console.log(username, age);
+
+		//* Reset form
+		setUsername("");
+		setAge("");
+	};
+
+	const changeUsernameHandler = (event) => {
+		//* Set the value of the username input to the value of the input
+		setUsername(event.target.value);
+	};
+
+	const changeAgeHandler = (event) => {
+		//* Set the value of the age input to the value of the input
+		setAge(event.target.value);
 	};
 
 	return (
 		<Card className={style.input}>
 			<form onSubmit={addUserHandler}>
 				<label htmlFor="username">Username</label>
-				<input type="text" id="username" name="username" />
+				<input
+					type="text"
+					id="username"
+					name="username"
+					value={username}
+					onChange={changeUsernameHandler}
+				/>
 				<label htmlFor="age">Age</label>
-				<input type="text" id="age" name="age" />
+				<input
+					type="number"
+					id="age"
+					name="age"
+					value={age}
+					onChange={changeAgeHandler}
+				/>
 				<Button type="submit">Add user</Button>
 			</form>
 		</Card>
